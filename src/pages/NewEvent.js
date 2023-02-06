1eddf7f984eed8cfb2481ec12df7ba0c24165422
Ka Hei Chan <domchan2000@gmail.com>
2023-02-06 12:32:28 -0500

    Revert "Added EventItem for adding events + styling"
    This reverts commit 967ea04eff64bad4959d6548b2bfe8aa8aa02646.

--- src/pages/NewEvent.js
+++ src/pages/NewEvent.js
@@ -1,30 +1,5 @@
-import { useNavigate } from "react-router-dom";
-
-import NewEventForm from "../components/events/NewEventForm";
-
 function NewEvent() {
-  const navigate = useNavigate();
-  function addEventHandler(EventData) {
-    fetch(
-      "https://react-getting-started-60b68-default-rtdb.firebaseio.com/events.json",
-      {
-        method: "POST",
-        body: JSON.stringify(EventData),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    ).then(() => {
-      navigate("/");
-    });
-  }
-
-  return (
-    <section>
-      <h1>New Event Page</h1>
-      <NewEventForm onAddEvent={addEventHandler} />
-    </section>
-  );
+  return <div>New Event Page</div>;
 }
 
 export default NewEvent;
