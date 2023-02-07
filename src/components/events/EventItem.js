ef146d4bfc3cc351594bf364d4c6404866488c5f
Ka Hei Chan <domchan2000@gmail.com>
2023-02-07 01:01:41 -0500

    Working on deleteHandler

--- src/components/events/EventItem.js
+++ src/components/events/EventItem.js
@@ -2,12 +2,18 @@ import classes from "./EventItem.module.css";
 import Card from "../UI/Card";
 
 function EventItems(props) {
+  function deleteHandler(){
+    
+  }
   return (
     <li className={classes.item}>
       <Card>
         <div className={classes.content}>
           <h3>{props.title}</h3>
           <p>{props.description}</p>
+        </div>
+        <div className= {classes.actions}>
+        <button onClick={deleteHandler}>Delete</button>
         </div>
       </Card>
     </li>
