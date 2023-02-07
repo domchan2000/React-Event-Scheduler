d0a79b1476dbb68ca912f7d9c8f65a3b08b4b033
Ka Hei Chan <domchan2000@gmail.com>
2023-02-07 00:20:14 -0500

    edited AllEvents Page to show list of events

--- src/pages/AllEvents.js
+++ src/pages/AllEvents.js
@@ -1,5 +1,47 @@
-function AllEvents() {
-  return <div>All Events Page</div>;
-}
+import { useState, useEffect } from "react";
 
+import EventList from "../components/events/EventList";
+
+function AllEvents() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedEvents, setloadedEvents] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-60b68-default-rtdb.firebaseio.com/events.json"
+    )
+      .then((respose) => {
+        return respose.json();
+      })
+      .then((data) => {
+        const events = [];
+        for (const key in data) {
+          const event = {
+            id: key,
+            ...data[key],
+          };
+
+          events.push(event);
+        }
+        setIsLoading(false);
+        setloadedEvents(events);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  return (
+    <div>
+      <h1>All Events Page</h1>
+      <EventList events={loadedEvents} />
+    </div>
+  );
+}
 export default AllEvents;
